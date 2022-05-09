bda620aa8fc76f948cc396e81346573e9ec12a17
quantumsheep <nathanael.dmc@outlook.fr>
2022-05-09 14:25:37 +0200

    Handle parsing errors

--- remixer/remixer.go
+++ remixer/remixer.go
@@ -1,9 +1,37 @@
 package remixer
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/antlr/antlr4/runtime/Go/antlr"
 	"github.com/tereus-project/tereus-remixer-c-go/parser"
 )
+
+type RemixerErrorListener struct {
+	filename string
+
+	Errors []string
+}
+
+func NewRemixerErrorListener(filename string) *RemixerErrorListener {
+	return &RemixerErrorListener{
+		filename: filename,
+	}
+}
+
+func (l *RemixerErrorListener) SyntaxError(recognizer antlr.Recognizer, offendingSymbol interface{}, line, column int, msg string, e antlr.RecognitionException) {
+	l.Errors = append(l.Errors, fmt.Sprintf("%s:%d:%d: %s", l.filename, line, column, msg))
+}
+
+func (l *RemixerErrorListener) ReportAmbiguity(recognizer antlr.Parser, dfa *antlr.DFA, startIndex, stopIndex int, exact bool, ambigAlts *antlr.BitSet, configs antlr.ATNConfigSet) {
+}
+
+func (l *RemixerErrorListener) ReportAttemptingFullContext(recognizer antlr.Parser, dfa *antlr.DFA, startIndex, stopIndex int, conflictingAlts *antlr.BitSet, configs antlr.ATNConfigSet) {
+}
+
+func (l *RemixerErrorListener) ReportContextSensitivity(recognizer antlr.Parser, dfa *antlr.DFA, startIndex, stopIndex, prediction int, configs antlr.ATNConfigSet) {
+}
 
 func Remix(entrypoint string) (string, error) {
 	preprocessor, err := NewPreprocessor(entrypoint)
@@ -25,9 +53,16 @@ func Remix(entrypoint string) (string, error) {
 	stream := antlr.NewCommonTokenStream(lexer, 0)
 	p := parser.NewCParser(stream)
 	p.Interpreter.SetPredictionMode(antlr.PredictionModeSLL)
-	p.AddErrorListener(antlr.NewDiagnosticErrorListener(true))
+	p.RemoveErrorListeners()
+
+	errorListener := NewRemixerErrorListener(entrypoint)
+	p.AddErrorListener(errorListener)
 
 	tree := p.Translation()
+
+	if len(errorListener.Errors) > 0 {
+		return "", fmt.Errorf("%s", strings.Join(errorListener.Errors, "\n"))
+	}
 
 	output, err := visitor.VisitTranslation(tree.(*parser.TranslationContext))
 	if err != nil {
