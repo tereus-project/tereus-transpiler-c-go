622b99d8f9058980da615b1cf4b077726261ac7e
Stanislas Lange <git@slange.me>
2022-06-10 14:22:43 +0200

    Update Env variables

--- env/env.go
+++ env/env.go
@@ -7,18 +7,18 @@ import (
 )
 
 type Env struct {
-	S3Bucket           string `env:"S3_BUCKET" env-required:"true"`
-	S3AccessKey        string `env:"S3_ACCESS_KEY" env-required:"true"`
-	S3SecretKey        string `env:"S3_SECRET_KEY" env-required:"true"`
-	S3Endpoint         string `env:"S3_ENDPOINT" env-required:"true"`
-	RabbitMQEndpoint   string `env:"RABBITMQ_ENDPOINT"`
-	KafkaEndpoint      string `env:"KAFKA_ENDPOINT" env-required:"true"`
+	S3Bucket    string `env:"S3_BUCKET" env-required:"true"`
+	S3AccessKey string `env:"S3_ACCESS_KEY" env-required:"true"`
+	S3SecretKey string `env:"S3_SECRET_KEY" env-required:"true"`
+	S3Endpoint  string `env:"S3_ENDPOINT" env-required:"true"`
+
 	NSQEndpoint        string `env:"NSQ_ENDPOINT" env-required:"true"`
 	NSQLookupdEndpoint string `env:"NSQ_LOOKUPD" env-required:"true"`
-	LogFormat          string `env:"LOG_FORMAT" env-default:"json"`
-	LogLevel           string `env:"LOG_LEVEL" env-default:"info"`
-	SentryDSN          string `env:"SENTRY_DSN"`
-	Env                string `env:"ENV" env-required:"true"`
+
+	LogFormat string `env:"LOG_FORMAT" env-default:"json"`
+	LogLevel  string `env:"LOG_LEVEL" env-default:"info"`
+	SentryDSN string `env:"SENTRY_DSN"`
+	Env       string `env:"ENV" env-required:"true"`
 
 	MetricsPort string `env:"METRICS_PORT" env-default:"8080"`
 }
