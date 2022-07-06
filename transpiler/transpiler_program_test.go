1aec581585926b81972f211e3be67911b0f27007
Stanislas Lange <git@slange.me>
2022-07-06 12:03:01 +0000

    Add `TestPrimeNumberBetweenTwoNumbersProgram`

--- transpiler/transpiler_program_test.go
+++ transpiler/transpiler_program_test.go
@@ -420,3 +420,98 @@ func main() {
 
 	assertTranspilation(t, source, target)
 }
+
+func TestPrimeNumberBetweenTwoNumbersProgram(t *testing.T) {
+	source := `
+#include <stdio.h>
+
+// user-defined function to check prime number
+int checkPrimeNumber(int n) {
+	int j, flag = 1;
+
+	for (j = 2; j <= n / 2; ++j) {
+
+	if (n % j == 0) {
+		flag = 0;
+		break;
+	}
+	}
+
+	return flag;
+}
+
+int main() {
+
+	int n1, n2, i, flag;
+
+	printf("Enter two positive integers: ");
+	scanf("%d %d", &n1, &n2);
+
+	// swap n1 and n2 if n1 > n2
+	if (n1 > n2) {
+	n1 = n1 + n2;
+	n2 = n1 - n2;
+	n1 = n1 - n2;
+	}
+
+	printf("Prime numbers between %d and %d are: ", n1, n2);
+	for (i = n1 + 1; i < n2; ++i) {
+
+	// flag will be equal to 1 if i is prime
+	flag = checkPrimeNumber(i);
+
+	if (flag == 1) {
+		printf("%d ", i);
+	}
+	}
+
+	return 0;
+}
+`
+	target := `
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/tereus-project/tereus-transpiler-c-go/libc"
+)
+
+// user-defined function to check prime number
+
+func checkPrimeNumber(n int) int {
+	j, flag := 0, 1
+	for j = 2; j <= n/2; libc.PreInc(&j) {
+		if n%j == 0 {
+			flag = 0
+			break
+		}
+	}
+	return flag
+}
+
+func main() {
+	n1, n2, i, flag := 0, 0, 0, 0
+	fmt.Printf("Enter two positive integers: ")
+	fmt.Scanf("%d %d", &n1, &n2)
+	// swap n1 and n2 if n1 > n2
+	if n1 > n2 {
+		n1 = n1 + n2
+		n2 = n1 - n2
+		n1 = n1 - n2
+	}
+	fmt.Printf("Prime numbers between %d and %d are: ", n1, n2)
+	for i = n1 + 1; i < n2; libc.PreInc(&i) {
+		// flag will be equal to 1 if i is prime
+		flag = checkPrimeNumber(i)
+		if flag == 1 {
+			fmt.Printf("%d ", i)
+		}
+	}
+	os.Exit(0)
+}
+`
+
+	assertTranspilation(t, source, target)
+}
